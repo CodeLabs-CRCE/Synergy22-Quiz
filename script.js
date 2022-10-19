90536bb1b65d3d4d536c5329049e274e8db309bd
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2022-10-19 23:03:09 +0530

    init

---
+++ script.js
@@ -0,0 +1,27 @@
+const topics = [
+    "Science",
+    "Art",
+    "History",
+    "Geography", 
+    "Movies and Cinema", 
+    "Maths", 
+    "General Knowledge", 
+    "Sports", 
+    "Astronomy",
+    "Music",
+    "Literature"];
+
+function getRandom(){
+    let selected = [];
+    while(selected.length<=2)
+    {
+        const newTopic = topics[Math.floor(Math.random() * topics.length)];
+        if(!selected.includes(newTopic))
+        {
+            selected.push(newTopic);
+        }
+    }
+    document.getElementById('1').innerText = selected[0];
+    document.getElementById('2').innerText = selected[1];
+    document.getElementById('3').innerText = selected[2];
+}
